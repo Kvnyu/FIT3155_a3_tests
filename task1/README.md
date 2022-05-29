fc0964e7abb723a59b2f6c24c29a363374f20dfc
Kvnyu <52982849+Kvnyu@users.noreply.github.com>
2022-05-29 12:33:45 +1000

    chore: format

--- task1/README.md
+++
@@ -1,2 +0,0 @@
-## Tests for task 2 of FIT3155 assignment 2
- - You should `cd` into the task1 folder before running these tests
--- task1/test.py
+++ task1/test.py
@@ -4,7 +4,6 @@ from filecmp import cmp
 
 three_primes_path = "/Users/kevin/Projects/FIT3155/assignments/assignment_3/task1/threeprimes.py"
 three_primes_path_parent = Path(three_primes_path).parent
-print(Path.cwd())
 
 for i in range(8, 1000):
   command = f'python {three_primes_path} {i}'
@@ -14,7 +13,7 @@ for i in range(8, 1000):
     raise Exception(f"Error with number {i}")
 
   # output = Path(three_primes_path_parent) / "output_threeprimes.txt"
-  with open(file="output_threeprimes.txt") as f:
+  with open(file=Path.cwd() / "output_threeprimes.txt") as f:
     line = f.readline().rstrip("\n").split(" ")
     if sum([int(x) for x in line]) != i:
       raise Exception(f"Error with number {i}")
