bfee499e3168a5d4a608334ad0cb63af6a8138c6
Kvnyu <52982849+Kvnyu@users.noreply.github.com>
2022-05-29 12:32:15 +1000

    chore: formatting

--- task2/test.py
+++ task2/test.py
@@ -10,7 +10,7 @@ myzip_path = "/Users/kevin/Projects/FIT3155/assignments/assignment_3/task2/myzip
 
 decoded_texts_path = Path(__file__).parent / 'decoded'
 encoded_texts_path = Path(__file__).parent / 'encoded'
-original_texts_path = Path(__file__).parent / 'original' 
+original_texts_path = Path(__file__).parent / 'original'
 
 exclude = ["empty.asc", "a10.asc", "s.asc"]
 print(Path.cwd())
@@ -20,7 +20,8 @@ if not Path(myunzip_path).is_file() or not Path(myzip_path).is_file():
 
 def test(exclude=[], window: int = 4, lookahead: int = 6):
   all_original_texts = original_texts_path.glob("*.asc")
-  original_file_paths = [file for file in all_original_texts if file.is_file() and file.name not in exclude]
+  original_file_paths = [file for file in all_original_texts if file.is_file()
+                         and file.name not in exclude]
 
   # Encode files
   for file_path in original_file_paths:
@@ -28,12 +29,12 @@ def test(exclude=[], window: int = 4, lookahead: int = 6):
     result = run(command.split(' '))
     print(f'Encoding: {file_path.name}')
     if result.returncode != 0:
-      raise Exception(f"Error with file {file_path.name} during encoding")
+      raise Exception(f"Error with file {file_path.name} uring encoding")
 
-  
   [move(file, encoded_texts_path / file.name) for file in Path.cwd().glob("*.asc.bin")]
   all_encoded_texts = encoded_texts_path.glob("*.asc.bin")
-  encoded_file_paths = [file for file in all_encoded_texts if file.is_file() and file.name not in exclude]
+  encoded_file_paths = [file for file in all_encoded_texts if file.is_file()
+                        and file.name not in exclude]
 
   # Decode files
   for file_path in encoded_file_paths:
@@ -44,7 +45,8 @@ def test(exclude=[], window: int = 4, lookahead: int = 6):
       raise Exception(f"Error with file {file_path.name} during decoding")
 
   [move(file, decoded_texts_path / file.name) for file in Path.cwd().glob("*.asc")]
-  decoded_file_paths = [file for file in Path.cwd().glob("*.asc") if file.is_file() and file.name not in exclude]
+  decoded_file_paths = [file for file in Path.cwd().glob(
+      "*.asc") if file.is_file() and file.name not in exclude]
 
   # Compare files
   for file_path in decoded_file_paths:
@@ -55,7 +57,8 @@ def test(exclude=[], window: int = 4, lookahead: int = 6):
       raise Exception(f"Error with file {file_path.name} during decoding")
 
   print("All tests pass!")
-  
+
+
 if __name__ == "__main__":
   # for window_size in range(0, 20, 4):
   #   for lookahead_size in range(0, 20, 4):
